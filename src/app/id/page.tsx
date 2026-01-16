e491d798057ef18d8b52e3efe19500b7305e1e98
rezkyyayang <rezky.yayang@gmail.com>
2026-01-16 21:48:49 +0700

    Edit bahasa version

--- src/app/id/page.tsx
+++ src/app/id/page.tsx
@@ -43,7 +43,7 @@ function HomeContent() {
             onClick={() => {
               const to = searchParams.get("to");
               const qs = to ? `?to=${encodeURIComponent(to)}` : "";
-              router.push(`https://sukmarezky.my.canva.site/digital-invitation`);
+              router.push(`https://sukmarezky.my.canva.site/undangandigital`);
             }}
           >
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-5 h-5">
